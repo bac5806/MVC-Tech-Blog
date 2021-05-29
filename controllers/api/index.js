77ada0670244bb2272d6a9a4af452d8bce75e37c
Broc C <bac5806@gmail.com>
2021-05-29 15:41:42 -0400

    added code for connecting to localhost

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// const userRoutes = require('./userRoutes');
+// const blogPost = require('./blogpost');
+
+// router.use('/users', userRoutes);
+// router.use('/blogposts', blogPost);
+
+module.exports = router;
